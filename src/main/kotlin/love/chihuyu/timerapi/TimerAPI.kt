e5b8f66bc86469e5f244c4058a704263a6ff648a
hirosukt <senolynn7584@gmail.com>
2023-06-16 03:57:18 +0900

    add run method

--- src/main/kotlin/love/chihuyu/timerapi/TimerAPI.kt
+++ src/main/kotlin/love/chihuyu/timerapi/TimerAPI.kt
@@ -1,5 +1,6 @@
 package love.chihuyu.timerapi
 
+import love.chihuyu.timerapi.timer.LoopTimer
 import love.chihuyu.timerapi.timer.Timer
 import org.bukkit.scheduler.BukkitTask
 
@@ -9,6 +10,18 @@ object TimerAPI {
 
     fun build(title: String, duration: Long, period: Long = 20, delay: Long = 0, builder: Timer.() -> Unit = {}): Timer {
         return Timer(title, duration, period, delay).apply(builder)
+    }
+
+    fun buildLoop(title: String, period: Long = 20, delay: Long = 0, builder: LoopTimer.() -> Unit = {}): LoopTimer {
+        return LoopTimer(title, period, delay).apply(builder)
+    }
+
+    fun run(title: String, duration: Long, period: Long = 20, delay: Long = 0, builder: Timer.() -> Unit = {}) {
+        Timer(title, duration, period, delay).apply(builder).run()
+    }
+
+    fun runLoop(title: String, period: Long = 20, delay: Long = 0, builder: LoopTimer.() -> Unit = {}) {
+        LoopTimer(title, period, delay).apply(builder).run()
     }
 
     fun all(): MutableList<BukkitTask> {
